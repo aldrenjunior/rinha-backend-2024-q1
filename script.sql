23d952577fbdfc6739b4abd24609271811e802c8
Aldrén Junior <vldrxn1@gmail.com>
2024-03-07 08:39:42 -0300

    add setup files

--- script.sql
+++ script.sql
@@ -1,13 +1,18 @@
 -- Coloque scripts iniciais aqui
-CREATE TABLE...
+CREATE TABLE clientes (
+    id SERIAL PRIMARY KEY NOT NULL,
+    nome VARCHAR(255),
+    limite INTEGER NOT NULL,
+    saldo INTEGER NOT NULL
+); 
 
 DO $$
 BEGIN
-  INSERT INTO clientes (nome, limite)
+  INSERT INTO clientes (nome, limite, saldo)
   VALUES
-    ('o barato sai caro', 1000 * 100),
-    ('zan corp ltda', 800 * 100),
-    ('les cruders', 10000 * 100),
-    ('padaria joia de cocaia', 100000 * 100),
-    ('kid mais', 5000 * 100);
+    ('o barato sai caro', 1000 * 100, 0),
+    ('zan corp ltda', 800 * 100, 0),
+    ('les cruders', 10000 * 100, 0),
+    ('padaria joia de cocaia', 100000 * 100, 0),
+    ('kid mais', 5000 * 100, 0);
 END; $$
